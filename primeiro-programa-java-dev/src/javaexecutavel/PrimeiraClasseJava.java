c3902078ccee0ff2ae79a92b369104304a0c7e78
usuario <usuario@DESKTOP-PJIS3FI>
2022-02-01 20:11:08 -0300

    Criação do Projeto

---
+++ primeiro-programa-java-dev/src/javaexecutavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package javaexecutavel;
+
+/*Main é um metodo auto escecutavwl em Java*/
+public class PrimeiraClasseJava {
+	public static void main(String[] args) {
+		
+		/*Ecreve texto no console*/
+		System.out.println("Meu Primeiro Programa em Java!! ");
+		
+	}
+
+}
